698a6b24456b15237eb1b5c23f2bf5d07b1d580d
Álvaro Neves <alvaro.neves2019@gmail.com>
2025-06-02 16:53:52 +0100

    Layouts - StackLayout

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -9,18 +9,9 @@ public partial class App : Application
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
-        var tabbedPage = new TabbedPageDemo();
-
-        var navPage = new NavigationPage(new MinhaPágina())
-        {
-            Title = "Início",
-            BarBackgroundColor = Colors.Yellow,
-            BarTextColor = Colors.Blue
-        };
-
-        tabbedPage.Children.Add(navPage);
-
-        return new Window(tabbedPage);
+        var navPage = new NavigationPage(new StackLayoutDemo());
+        
+        return new Window(navPage);
     }
 }
 
